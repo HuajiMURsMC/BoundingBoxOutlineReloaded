c21ec8e0828966a9e80b2d20ecf618b71be13008
Wagyourtail <wagyourtail@wagyourtail.xyz>
2022-01-23 02:38:55 -0700

    remove unnecessary priority change

--- src/main/java/com/irtimaled/bbor/mixin/client/MixinKeyboard.java
+++ src/main/java/com/irtimaled/bbor/mixin/client/MixinKeyboard.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Keyboard.class, priority = 1050)
+@Mixin(value = Keyboard.class)
 public class MixinKeyboard {
 
     /**
