d700fc8e6df3868b58c4a147a36e7d46defaf40f
Huaji_MUR233 <huajimur@jvav.me>
2022-03-13 21:40:19 +0800

    fix a bug

--- src/main/java/com/irtimaled/bbor/mixin/client/settings/MixinKeyBinding.java
+++ src/main/java/com/irtimaled/bbor/mixin/client/settings/MixinKeyBinding.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.Map;
 import java.util.Set;
 
 @Mixin(KeyBinding.class)
@@ -13,7 +14,10 @@ public class MixinKeyBinding {
 
     @Shadow @Final private static Set<String> KEY_CATEGORIES;
 
+    @Shadow @Final private static Map<String, Integer> CATEGORY_ORDER_MAP;
+
     static {
         KEY_CATEGORIES.add(KeyListener.Category);
+        CATEGORY_ORDER_MAP.put(KeyListener.Category, CATEGORY_ORDER_MAP.size() + 1);
     }
 }
